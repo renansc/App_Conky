a2f6073ce7f83b198812e3fce9651dcc33d8a8d0
Renan Santos Coutinho <renanrocks2009@gmail.com>
2020-05-22 08:52:35 -0300

    atualizações no instalador
    instalando dependências também

--- conky_install.sh
+++ conky_install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#instala o conky
+echo "instalando pacote do conky"
+sudo apt-get install conky-all
+#instala censores
+echo "instalando pacote de sensores lm-sensors"
+sudo apt-get install lm-sensors
+echo "instalando suporte aplet do sensors"
+sudo apt-get install sensors-applet
+echo "configurando sensores"
+sudo sensors-detect --yes
+echo "executando monitoramento de sensores"
+sudo sensors &&
+#instala o curl
+echo "instalando pacote curl"
+sudo apt-get install curl
 #busca no arquivo os dados salvos.
 echo "iniciando configuração"
 echo "copiando arquivo de configuração adaptador de rede"
